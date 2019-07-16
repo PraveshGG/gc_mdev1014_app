1fbbf72104aaafd224583410065ca17de5060c28
MosCD <moscd3@gmail.com>
2019-07-16 00:19:52 -0400

    commit

--- firstapp.Android/Resources/Resource.designer.cs
+++ firstapp.Android/Resources/Resource.designer.cs
@@ -26,6 +26,10 @@ namespace firstapp.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::PCLCrypto.Resource.String.ApplicationName = global::firstapp.Droid.Resource.String.ApplicationName;
+			global::PCLCrypto.Resource.String.Hello = global::firstapp.Droid.Resource.String.Hello;
+			global::PCLStorage.Resource.String.ApplicationName = global::firstapp.Droid.Resource.String.ApplicationName;
+			global::PCLStorage.Resource.String.Hello = global::firstapp.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_fade_in = global::firstapp.Droid.Resource.Animation.abc_fade_in;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_fade_out = global::firstapp.Droid.Resource.Animation.abc_fade_out;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_grow_fade_in_from_bottom = global::firstapp.Droid.Resource.Animation.abc_grow_fade_in_from_bottom;
@@ -6012,6 +6016,12 @@ namespace firstapp.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f0a003d
+			public const int ApplicationName = 2131361853;
+			
+			// aapt resource value: 0x7f0a003c
+			public const int Hello = 2131361852;
 			
 			// aapt resource value: 0x7f0a0015
 			public const int abc_action_bar_home_description = 2131361813;
